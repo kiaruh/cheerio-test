6d9a7849b4cf0eb5eb9b6074a6286a2a5916e8d2
kiaruh <nikoz.li@gmail.com>
2023-03-14 12:49:27 -0300

    infobae test

--- cheerio-test1.js
+++ cheerio-test1.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
 
-const url = "https://www.mercadolibre.com.ar/";
+const url = "https://www.infobae.com/opinion/2023/03/06/no-vuelvas-messi/";
 
 request(url, (error, response, html) => {
   if (!error && response.statusCode == 200) {
